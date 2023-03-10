85711b3d567207c71b72b11ca991f6850670745a
enipx <progenipx@gmail.com>
2023-03-10 23:58:43 +0100

    chore: improvement

--- web/src/components/view/view.stories.tsx
+++ web/src/components/view/view.stories.tsx
@@ -1,26 +1,24 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
+import { Text } from '../text';
 import { View } from './view';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/View',
   component: View,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof View>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof View> = (args) => <View {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  backgroundColor: 'blue.500',
+  bg: 'gray.100',
   size: '400px',
-  children: <p>Hi, My name is Hashir</p>,
-  flexCenterY: true,
+  children: <Text color="gray.500">This is a view</Text>,
+  flexCenter: true,
   p: 'lg',
+  mx: 'auto',
 };
