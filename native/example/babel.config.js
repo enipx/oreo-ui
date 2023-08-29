0678917824e300624be4a946660eee51d67d2a3d
enipx <progenipx@gmail.com>
2023-08-29 06:22:28 +0100

    chore: improvements [native]

--- native/example/babel.config.js
+++ native/example/babel.config.js
@@ -13,7 +13,9 @@ module.exports = function (api) {
           extensions: ['.tsx', '.ts', '.js', '.json'],
           alias: {
             // For development, we want to alias the library to the source
-            [pak.name]: path.join(__dirname, '..', pak.source)
+            [pak.name]: path.join(__dirname, '..', pak.source),
+            ['@']: path.join(__dirname, '../../'),
+            ['@components']: path.join(__dirname, '../src/components/'),
           },
         },
       ],
